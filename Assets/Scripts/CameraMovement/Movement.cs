24371fd82f136d5fea058dea9d2cf25a485200e0
Tatyana <ta59ney@rambler.ru>
2022-12-26 20:24:02 +0300

    Changed

--- Assets/Scripts/CameraMovement/Movement.cs
+++ Assets/Scripts/CameraMovement/Movement.cs
@@ -25,16 +25,11 @@ public class Movement : MonoBehaviour
 
     private void Start()
     {
-        _runningTime = 1;
+        _runningTime = 0.003f;
         _distanceMoving = 3;
         _rigidbody2D = GetComponent<Rigidbody2D>();
 
         ResetMarshmallowCat();
-    }
-
-    private void Update()
-    {
-        StartCoroutine(Move());
     }
 
     public void ResetMarshmallowCat()
@@ -45,28 +40,28 @@ public class Movement : MonoBehaviour
     private void OnMovementChanged()
     {
         _isMove = true;
+
+        StartCoroutine(Move(_runningTime));
     }
 
-    private IEnumerator Move()
+    private IEnumerator Move(float runningTime)
     {
-        var waitForSeconds = new WaitForSeconds(_runningTime);
+        _duration = 0;
 
-        while (_isMove)
+        while(_isMove)
         {
-            if (_duration <= _runningTime)
+            yield return new WaitForSeconds(runningTime);
+
+            _duration++;
+
+            _rigidbody2D.MovePosition(Vector3.Lerp(_currentPosition, _target.position, runningTime));
+
+            if (_duration > runningTime)
             {
-                _duration += Time.deltaTime;
-                _rigidbody2D.MovePosition(Vector3.Lerp(_currentPosition, _target.position, _duration));
-            }
-            else
-            {
-                _currentPosition = transform.position;
                 _target.position += Vector3.up * _distanceMoving;
                 _isMove = false;
                 _duration = 0;
             }
-
-            yield return waitForSeconds;
         }
     }
 }
